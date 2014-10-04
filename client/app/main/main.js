b7ade50bded51179ed33047cf34b194534692af0
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-10-03 20:36:34 -0500

    initial commit from Yeoman angular-fullstack

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('censusApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
